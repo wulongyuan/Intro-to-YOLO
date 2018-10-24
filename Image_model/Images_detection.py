454f74a1c663733dc9ae195e8066d96427242911
Santosh Muniswamygowda Nagaraja <31931121+santoshmn26@users.noreply.github.com>
2018-10-24 09:56:22 -0500

    Update Images_detection.py

--- Image_model/Images_detection.py
+++ Image_model/Images_detection.py
@@ -5,24 +5,24 @@ import numpy as np
 
 
 options = {
-    'model': 'cfg/yolov3.cfg',                                                              # yolo pre-trained model
-    'load': bin/yolov3.weights,                                                             # yolov3 weights
-    'threshold': 0.15
+    'model': 'cfg/yolov3.cfg',                                                      # yolo pre-trained model
+    'load': bin/yolov3.weights,                                                     # yolov3 weights
+    'threshold': 0.15                                                               # Threshold, better trained can update to higher value
 }
 
 tfnet = TFNet(options)
-i=75
+i=0
 colors = [tuple(255 * np.random.rand(3)) for i in range(10)]
-for i in range(76,300):
+for i in range(0,300):                                              # Images_0 to Images_300, depends on how many images you have
     try:
         img_path="C:\\path\\image"+str(i)+".jpg"                                            #Change the path
         img = cv2.imread(img_path, cv2.IMREAD_COLOR)
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        results = tfnet.return_predict(img)
-        for color, result in zip(colors, results):
+        results = tfnet.return_predict(img)                         # Get the predection from the tfnet
+        for color, result in zip(colors, results):                  # Zip each result with a color to draw the bounding box
             print(result['label'],"{:.2f}".format(result['confidence']*100))
-            if(result['confidence']<0.1):
-                continue
+            if(result['confidence']<0.1):                           # Confidence level of 0.1 (10%).
+                continue                                            # Change the value from 0.1 to 1 to see changes in the output.
             else:
                 tl = (result['topleft']['x'], result['topleft']['y'])
                 br = (result['bottomright']['x'], result['bottomright']['y'])
