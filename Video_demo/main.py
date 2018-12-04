c29e2c3239ea3618598e2003e7d4237f5592d766
Santosh M N <31931121+santoshmn26@users.noreply.github.com>
2018-12-04 12:18:20 -0600

    Update main.py

--- Video_demo/main.py
+++ Video_demo/main.py
@@ -10,34 +10,34 @@ import threading
 option = {
     'model': 'cfg/tiny-yolo-voc-3c.cfg',  # Model to load
     'load': -1,                           # Weights to load
-    'threshold': 0.4,                     # Threshold value
-    'gpu': 0                              # GPU value (0 - 1)         
+    'threshold': 0.4,                     # Threshold value, Confidence level of the object detected
+    'gpu': 0                              # GPU value (0 - 1)      if tensorflow GPU is installed   
 }
 
 video_source='camera'
 
 
-def write_data(result):
-    f=open("log.txt",'a+')
+def write_data(result):                 # Storing the details of the objects detected in a text file
+    f=open("log.txt",'a+')              # Objects detected storing in a text file for now, later updates will store in database (MangoDB)
     for i in results:
         f.write(str(datetime.datetime.now())+','+i['label']+','+str(i['confidence'])+','+str(i['topleft']['x'])+','+str(i['topleft']['y'])+','+str(i['bottomright']['x'])+','+str(i['bottomright']['y'])+','+video_source+'\n')
         f.close
 
 tfnet = TFNet(option)
 capture = cv2.VideoCapture("junk/1 (9).mp4")        # Place your path to your video file, or assign 0 to load webcam feed.
-fourcc = cv2.VideoWriter_fourcc(*'XVID')
+fourcc = cv2.VideoWriter_fourcc(*'XVID')            # Comment this line if you dont want to save the output video
 
-initial_thread_count = threading.enumerate()
-print(len(initial_thread_count))
-while (capture.isOpened()):
-    stime = time.time()
-    ret, frame = capture.read()
+initial_thread_count = threading.enumerate()        # Checking the current thread count
+# print(len(initial_thread_count))
+while (capture.isOpened()):                         # Checking if the video sources is generating frames
+    stime = time.time()                             # Current time to record in the database
+    ret, frame = capture.read()                     # Splittting the video feed into frames
     if ret:
-        thread_count = threading.enumerate()
-        results = tfnet.return_predict(frame)
+        thread_count = threading.enumerate()        # Checking the latest thread count
+        results = tfnet.return_predict(frame)       # Frame is sent to tfnet which intern returns a list of objects detected in the frame
         if (len(thread_count)==len(initial_thread_count)):
             threading.Timer(5.0, write_data,args=(results,)).start()    
-        for result in results:
+        for result in results:                      # Loop throught the list of objects detected in the current frame
             if(result['label']=='Person'):
                 frame = cv2.rectangle(frame, (result['topleft']['x'], result['bottomright']['y']), (result['bottomright']['x'], result['bottomright']['y']+30), (0,255,255),3, cv2.FILLED)
             else:
@@ -53,7 +53,7 @@ while (capture.isOpened()):
 
                 frame = cv2.putText(frame, result['label'],((result['topleft']['x']),(result['bottomright']['y'])+30), cv2.FONT_HERSHEY_DUPLEX, 1, (0,0,0), 1)
                 
-        cv2.imshow('frame', frame)
+        cv2.imshow('frame', frame)      # Display the output for each frame
         #out.write(frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
